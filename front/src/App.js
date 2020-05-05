70bd3f1de6ae4b9ef844ebd9873f5e5849b65822
aditya0072001 <tripathi.aditya1660@gmail.com>
2020-05-05 18:56:09 +0530

    added project files

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          And it comes <code>React.js + Django</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
