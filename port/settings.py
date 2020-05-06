b4eb6069a0a495d02d53ce629ca2baad45bb996e
aditya0072001 <tripathi.aditya1660@gmail.com>
2020-05-06 23:00:10 +0530

    static files

--- port/settings.py
+++ port/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR+'front/build/static'
+STATIC_ROOT=os.path.join(BASE_DIR,'front/build/staticfiles')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'front/build/static')
 ]
