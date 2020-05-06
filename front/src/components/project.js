bab6e6ac7772378ec30ddb697d4ca102781f3776
aditya0072001 <tripathi.aditya1660@gmail.com>
2020-05-06 19:20:28 +0530

    added footer with social links

--- front/src/components/project.js
+++ front/src/components/project.js
@@ -3,6 +3,12 @@ import './project.css';
 class Project extends React.Component {
     render() {
       return <div>
+      <a href="https://drive.google.com/file/d/1zZ2w-Jujwz5TQsXFEh8CsRtRoq4kJTHD/view?usp=sharing" class="buttonDownload">Resume</a>
+      <br/>
+      <div class="wr">
+        <div class="linkedin icon"><a href="https://www.linkedin.com/in/tripathi-aditya-prakash-95316b191/" target="_blank"></a></div>
+        <div class="github icon"><a href="https://github.com/aditya0072001" target="_blank"></a></div>
+      </div>
     </div>
     }
   }
