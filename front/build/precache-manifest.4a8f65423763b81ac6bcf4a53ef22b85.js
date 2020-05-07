f5400c819497a8e59ecec918c5c348c38543a7e6
Tripathi Aditya Prakash <41997646+aditya0072001@users.noreply.github.com>
2020-05-07 00:19:17 -0700

    build files added

---
+++ front/build/precache-manifest.4a8f65423763b81ac6bcf4a53ef22b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe9b8501930b065f9c9387bd27fa0d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0626e16c79e30d43843c",
+    "url": "/static/css/main.830ea747.chunk.css"
+  },
+  {
+    "revision": "6998cf0a28a428ac9021",
+    "url": "/static/js/2.56d53051.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56d53051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0626e16c79e30d43843c",
+    "url": "/static/js/main.21b8278b.chunk.js"
+  },
+  {
+    "revision": "92273b49d3690110aa39",
+    "url": "/static/js/runtime-main.584e3335.js"
+  }
+]);
